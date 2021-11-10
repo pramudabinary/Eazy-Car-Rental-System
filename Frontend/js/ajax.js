81553aa224bb784b5f1e3710e08799f234ae6430
Pramuda Liyanage <pramudatharika@gmail.com>
2021-11-10 18:32:58 +0530

    Frontend  LoginForm and SignUp Form

--- Frontend/js/ajax.js
+++ Frontend/js/ajax.js
@@ -1,5 +1,5 @@
 // ============================Registration===================
-$("#btnRegister").click(function (){
+$("#btnRegister").click(function () {
 
     let customerID = $("#customerId").val();
     let customerName = $("#customerName").val();
@@ -27,53 +27,52 @@ $("#btnRegister").click(function (){
             license: customerLicense,
         }),
         success: function (data) {
-            registerValidation(parse);
+           alert("SignUp Successful!!")
         }
 
     })
-})
+});
+
 // =========================Register Success Popup Box===================
-function registerValidation(parse){
-    if(parse){
-        Swal.fire(
-            'Good Job!',
-            'You are now a new customer!',
-            'success'
-        );
-    }else{
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Something went to wrong!',
-        })
-    }
-}
 
 // ============================Login===================
-$("#loginBtn").click(function (){
+$("#loginBtn").click(function () {
 
-    let customerEmail = $("#email").val();
-    let customerPassword = $("#password").val();
+    let userName = $("#exampleInputUserName").val();
+    let password = $("#exampleInputPassword1").val();
+
 
     $.ajax({
-        method: "POST",
-        url: "http://localhost:8080/Backend_war/api/v1/customer/login",
+        method: 'POST',
+        url: "http://localhost:8080/Backend_war/api/v1/admin/oauth/token",
         contentType: 'application/json',
         async: true,
         data: JSON.stringify({
-            email: customerEmail,
-            password: customerPassword,
+            username:userName,
+            password:password,
         }),
-        success: function () {
-            if (true){
-                $("#dashboard").css('display', 'block');
-            }else{
-                alert("Error");
-            }
+        success: function (data) {
+            $("#loginform").css("display", "none");
+            $("#dashboard").css("display", "block");
         }
+    });
+// }else{
+    // $.ajax({
+    //     method: 'POST',
+    //     url: "http://localhost:8080/Backend_war/api/v1/customer/loginfu",
+    //     contentType: 'application/json',
+    //     async: true,
+    //     data: data,
+    //     success: function (data) {
+    //         alert("Customer");
+    //     }
+    //
+    //
+    // })}
+});
 
-    })
-})
+
+
 // ============================File Upload===================
 var fileObject = $("#file")[0].files[0];//access file object from input field
 var fileName = $("#file")[0].files[0].name; //get file name
--- Frontend/js/controls.js
+++ Frontend/js/controls.js
@@ -7,6 +7,8 @@ $("#btnHome").click(function () {
     $("#loginform").css("display", "none");
     $("#home").css("display", "block");
     $("#dashboard").css("display", "none");
+    $("#cars").css("display", "none");
+    $("#admin").css("display", "none");
 });
 
 $("#btnSignUp").click(function () {
@@ -14,6 +16,8 @@ $("#btnSignUp").click(function () {
     $("#register").css("display", "block");
     $("#loginform").css("display", "none");
     $("#dashboard").css("display", "none");
+    $("#cars").css("display", "none");
+    $("#admin").css("display", "none");
 });
 
 $("#btnSignIn").click(function () {
@@ -21,15 +25,43 @@ $("#btnSignIn").click(function () {
     $("#register").css("display", "none");
     $("#loginform").css("display", "block");
     $("#dashboard").css("display", "none");
-});
+    $("#cars").css("display", "none");
+    $("#admin").css("display", "none");
+})
+
+$("#btnHomeRent").click(function () {
+    $("#home").css("display", "none");
+    $("#register").css("display", "none");
+    $("#loginform").css("display", "block");
+    $("#dashboard").css("display", "none");
+    $("#cars").css("display", "none");
+    $("#admin").css("display", "none");
+})
+
+$("#btnHave").click(function () {
+    $("#home").css("display", "none");
+    $("#register").css("display", "none");
+    $("#loginform").css("display", "block");
+    $("#dashboard").css("display", "none");
+    $("#cars").css("display", "none");
+    $("#admin").css("display", "none");
+})
 
 function hideAll() {
-    $("#home, #loginform ,#register,#dashboard").css('display', "none");
+    $("#home, #cars ,admin, #loginform ,#register,#dashboard").css('display', "none");
 }
 
 // Btns Control
+
+// Customer Nav Btn
 
 $("#btnPay").css('display', 'none');
 $("#btnCars").css('display', 'none');
 $("#btnBook").css('display', 'none');
 $("#btnLogOut").css('display', 'none');
+
+// Admin Nav Btn
+
+$("#cusManage").css('display', 'none');
+$("#carManage").css('display', 'none');
+$("#bookManage").css('display', 'none');
